170e0f1aff697c9bbeadad235179bbccdbcbe3b9
igor <igorkaktus@gmail.com>
2023-11-08 12:02:24 +0000

    Added Modal Page with Reanimated

--- delivery_mobile_app/app/_layout.tsx
+++ delivery_mobile_app/app/_layout.tsx
@@ -1,6 +1,10 @@
 import { Stack } from 'expo-router';
 import CustomHeader from '@/components/CustomHeader';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
+import Colors from '@/constants/Colors';
+import { TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
@@ -9,7 +13,7 @@ export const unstable_settings = {
 
  export default function RootLayoutNav() {
 
-
+const navigation = useNavigation();
   return (
     <BottomSheetModalProvider>
       <Stack>
@@ -18,6 +22,23 @@ export const unstable_settings = {
         options={{ 
           header: () => <CustomHeader/>
          }} />
+         <Stack.Screen 
+         name="(modal)/filter"
+         options={{
+          presentation: 'modal',
+          headerTitle: 'Filter',
+          headerShadowVisible: false,
+          headerStyle:{
+            backgroundColor: Colors.lightGrey
+          },
+          headerLeft: () => (
+            <TouchableOpacity onPress={navigation.goBack}>
+              <Ionicons name='close-outline' size={28} color={Colors.primary}/>
+              </TouchableOpacity>
+          )
+          
+         }}
+         />
       </Stack>
       </BottomSheetModalProvider>
   );
--- delivery_mobile_app/components/CustomHeader.tsx
+++ delivery_mobile_app/components/CustomHeader.tsx
@@ -14,7 +14,7 @@ const SearchBar = () => (
                 <Ionicons  style={styles.searchIcon } name="ios-search" size={20} color={Colors.medium}/>
                 <TextInput style={styles.input} placeholder='Restaurants, groceries, dishes'/>
                 </View>
-                <Link href={'/'} asChild>
+                <Link href={'/(modal)/filter'} asChild>
             <TouchableOpacity style={styles.optionButton}>
                 <Ionicons name='options-outline' size={20} color={Colors.primary}/>
             </TouchableOpacity>
