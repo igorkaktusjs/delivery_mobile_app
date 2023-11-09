2435c7f2ad19a01bccae5abb35247154cdfb8d48
igor <igorkaktus@gmail.com>
2023-11-09 12:04:42 +0000

    Added Horizontal Scrolling

--- delivery_mobile_app/app/index.tsx
+++ delivery_mobile_app/app/index.tsx
@@ -1,12 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import React from "react";
+import Categories from "@/components/Categories";
+import Restaurants from "@/components/Restaurants";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Colors from "@/constants/Colors";
 
 const Page = () => {
   return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={{ paddingBottom: 40 }}>
+        <Categories />
+        <Text style={styles.header}> Top picks in your neighourhood</Text>
+        <Restaurants />
+        <Text style={styles.header}> Offers near you</Text>
+        <Restaurants />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
+const styles = StyleSheet.create({
+  container: {
+    top: 80,
+    backgroundColor: Colors.lightGrey,
+  },
+  header: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 8,
+    paddingHorizontal: 16,
+    marginTop: 16,
+  },
+});
 export default Page;
---
+++ delivery_mobile_app/components/Restaurants.tsx
@@ -0,0 +1,82 @@
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { restaurants } from "@/assets/data/home";
+import { Link } from "expo-router";
+import Colors from "@/constants/Colors";
+
+const Restaurants = () => {
+  return (
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{
+        padding: 15,
+      }}
+    >
+      {restaurants.map((restaurants, index) => (
+        <Link href={"/"} key={index} asChild>
+          <TouchableOpacity>
+            <View style={styles.categoryCard}>
+              <Image source={restaurants.img} style={styles.image} />
+              <View style={styles.categoryBox}>
+                <Text style={styles.categoryText}>{restaurants.name}</Text>
+                <Text style={styles.categoryRating}>
+                  {restaurants.rating} {restaurants.ratings}
+                </Text>
+                <Text style={styles.categoryDistance}>
+                  {restaurants.distance}{" "}
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </Link>
+      ))}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  categoryCard: {
+    width: 300,
+    height: 250,
+    backgroundColor: "#fff",
+    marginEnd: 10,
+    elevation: 2,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.06,
+    borderRadius: 4,
+  },
+  categoryText: {
+    paddingVertical: 5,
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  image: {
+    flex: 5,
+    width: undefined,
+    height: undefined,
+  },
+  categoryBox: {
+    flex: 2,
+    padding: 10,
+  },
+  categoryRating: {
+    color: Colors.green,
+  },
+  categoryDistance: {
+    color: Colors.medium,
+  },
+});
+
+export default Restaurants;
